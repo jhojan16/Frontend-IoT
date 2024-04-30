f26e5c46d64a159a540dae7c84a64bc629533f22
{HANA0K4} <{mateohanaoka@gmail.com}>
2024-04-30 16:44:22 -0500

    se borro

--- frontendVite/src/api/nodos.js
+++
@@ -1,8 +0,0 @@
-export const ManageNodosService = {
-    baseUrl: import.meta.env.VITE_IOT_NODOS,
-    endpoints: {
-        getNodos: '/',
-        getPeso: '/datosPeso',
-        getUltrasonido: '/datosUltrasonido',
-    },
-};
--- frontendVite/src/api/users.js
+++
@@ -1,8 +0,0 @@
-export const AuthService = {
-    baseUrl: import.meta.env.VITE_IOT,
-    endpoints: {
-        login: '/login',
-        register: '/register',
-        getNodos: '/',
-    },
-};
