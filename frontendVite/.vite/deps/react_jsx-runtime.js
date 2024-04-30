542e9556b5e9724bc936a2c8015e75af5696b97d
Jhojan <147114192+jhojan16@users.noreply.github.com>
2024-04-30 11:24:06 -0500

    fix structure

--- frontendVite/.vite/deps/react_jsx-runtime.js
+++
@@ -1,7 +0,0 @@
-import {
-  require_jsx_runtime
-} from "./chunk-AANCVKBC.js";
-import "./chunk-UM3JHGVO.js";
-import "./chunk-CEQRFMJQ.js";
-export default require_jsx_runtime();
-//# sourceMappingURL=react_jsx-runtime.js.map
